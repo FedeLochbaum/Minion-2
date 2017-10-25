d4ec7382f91b493907385e8d7db36930af32ff7f
root <federico.lochbaum@gmail.com>
2017-10-25 20:37:56 -0300

    calculo de defensa

--- Assets/Scripts/Entity/Stats/DefensiveCalculator.cs
+++ Assets/Scripts/Entity/Stats/DefensiveCalculator.cs
@@ -8,11 +8,13 @@ public class DefensiveCalculator : Calculator {
 	}
 
 	public float reduceMagicalDamage(float damage){
-		return 1;
+		// aplicar decremento con efectos
+		return damage - (stats.getInt () + stats.getVit () / 5 + stats.getDex () / 5 + stats.level () / 4);
 	}
 
 	public float reducePhysicalDamage(float damage){
-		return 1;
+		// aplicar decremento con efectos
+		return damage - (stats.getVit / 2) + Mathf.Max((stats.getVit() * 0.3), (stats.getVit() * stats.getVit() / 150) - 1);
 	}
 
 	public override void update(){
