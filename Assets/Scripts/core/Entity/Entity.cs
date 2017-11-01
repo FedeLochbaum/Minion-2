5e33e607ae764074dc2d6a3fa3491eeb92557462
root <federico.lochbaum@gmail.com>
2017-10-31 23:06:52 -0300

    se agrego a entity el llamado a finish turn faltante

--- Assets/Scripts/core/Entity/Entity.cs
+++ Assets/Scripts/core/Entity/Entity.cs
@@ -7,11 +7,13 @@ public abstract class Entity : MonoBehaviour {
 	protected string entityName;
 	public Stats stats; 
 	protected ActionSystem actionSystem;
+	protected GameSystem gameSystem;
 
 	public Entity (string name) {
 		entityName = name;
 		stats = new Stats ();
 		actionSystem = GameObject.FindObjectOfType<ActionSystem> ();
+		gameSystem = GameObject.FindObjectOfType<GameSystem> ();
 	}
 
 	public string getName(){
@@ -30,6 +32,7 @@ public abstract class Entity : MonoBehaviour {
 	public void myTurn(){
 		selectAction ();
 		stats.updateTurn ();
+		gameSystem.finishTurnPlayer ();
 	}
 
 	public abstract void selectAction ();
